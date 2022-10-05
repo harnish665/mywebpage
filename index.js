105b633620611d382fd67d5c0436e44155f4a448
harnish665 <84714021+harnish665@users.noreply.github.com>
2022-10-05 19:01:20 +0100

    update js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-document.querySelector("button").addEventListener("click", myFunction);
-function myFunction() {
-  alert("A warm welcome 🥰");
-}
+  document.querySelector("button").addEventListener("click", clickHandler);
+  function clickHandler() {
+    document.querySelector(".love").classList.toggle("i")
+  }
